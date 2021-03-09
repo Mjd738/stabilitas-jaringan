345efd2bd879caafcf1685d3a86b971a23c99845
Mjd738 <80350263+Mjd738@users.noreply.github.com>
2021-03-10 02:39:56 +0700

    Add files via upload

---
+++ tes.sh
@@ -0,0 +1,42 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yelow='\033[33;1m'
+sleep 1
+echo 
+toilet -f big -f gay BY MAJID TSALIS
+echo $red"Welcome bro!"
+echo
+sleep 3
+echo $yelow"pilih salah satu tools di bawah ini! :"
+echo
+echo $purple"1).stabilkan jaringan"
+echo
+echo $green"2.hacking (soon)"
+echo
+echo $cyan"0.out"
+echo
+echo $red
+read -p "pilihMana? : " bro
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -f gay BY MAJID TSALIS
+echo $green"stabilkan jaringan by majid"
+sleep 2
+ping -s1000 1.1.1.1
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+echo $yellow
+figlet "Bye"
+echo $white"To Be Continued"
+sleep 3
+exit
+fi
